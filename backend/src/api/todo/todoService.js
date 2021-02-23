0b19d92698aa4ce977369245d87dd65f37015d7b
Dan Silva <daniel.730@outlook.com>
2021-02-23 16:13:06 -0300

    Configuração inicial backend

---
+++ backend/src/api/todo/todoService.js
@@ -0,0 +1,15 @@
+const Todo = require("./todo")
+
+Todo.methods([
+    'get', 
+    'post', 
+    'put', 
+    'delete'
+])
+
+Todo.updateOptions({
+    new: true,
+    runValidators: true
+})
+
+module.exports = Todo
